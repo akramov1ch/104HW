1a42ab797cb9938218e00adc50ec2fbe9f1f3518
akramov1ch <shaxbozakramovic@gmail.com>
2024-09-18 11:24:40 +0500

    main

---
+++ cmd/main.go
@@ -0,0 +1,45 @@
+
+package main
+
+import (
+	"104HW/config"
+	"104HW/internal/handler"
+	"104HW/internal/repository"
+	"104HW/internal/service"
+	"104HW/pkg/database"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := database.ConnectPostgres()
+	if err != nil {
+		log.Fatal("Could not connect to the database:", err)
+	}
+
+	userRepo := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepo)
+	authHandler := handler.AuthHandler{UserService: userService}
+	userHandler := handler.UserHandler{UserService: userService}
+
+	router := gin.Default()
+
+	router.POST("/api/login", authHandler.Login)
+
+	router.POST("/api/users", userHandler.CreateUser)
+	router.GET("/api/users/:id", userHandler.GetUserByID)
+	router.PUT("/api/users/:id", userHandler.UpdateUser)
+	router.DELETE("/api/users/:id", userHandler.DeleteUser)
+
+	port := config.GetEnv("PORT", "8080")
+	if err := router.Run(":" + port); err != nil {
+		log.Fatal("Could not run server:", err)
+	}
+}
